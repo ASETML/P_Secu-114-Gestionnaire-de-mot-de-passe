97be627f9e4498a9593b68359f50bf9feba8b3c4
ASETML <alban.segalen@eduvaud.ch>
2024-11-13 13:37:08 +0100

    feat(code): Added main menu
    WIP

--- P_Secu-114/P_Secu-114/Program.cs
+++ P_Secu-114/P_Secu-114/Program.cs
@@ -4,12 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Menu mainMenu = new Menu();
-
+            int choice = 0;
+            /*Console.WriteLine("Enter MasterPassword");
             MasterPassword.Key = Console.ReadLine();
-            Console.WriteLine(MasterPassword.Key);
+            Console.WriteLine(EncryptionManager.Decrypt(File.ReadAllText("pwd.txt")));
+
             File.WriteAllText("pwd.txt", EncryptionManager.Encrypt(Console.ReadLine()));
             Console.WriteLine(EncryptionManager.Decrypt(File.ReadAllText("pwd.txt")));
-           }
+            */
+
+            do
+            {
+                Console.WriteLine("**************************************************************************************");
+                Console.WriteLine("1. Consulter un mot de passe");
+                Console.WriteLine("2. Ajouter un mot de passe");
+                Console.WriteLine("3. Supprimer un mot de passe");
+                Console.WriteLine("4. Quitter le programme");
+                Console.WriteLine("**************************************************************************************");
+                bool b = true;
+                Console.WriteLine("a");
+            } while (true);
+        }
     }
 }
