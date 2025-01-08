1809af6c6026f9f67394ef3c8db114db07f6b2b7
ASETML <alban.segalen@eduvaud.ch>
2025-01-08 17:34:28 +0100

    Information de debug supprimée de Program.cs

--- P_Secu-114-WinForms/P_Secu-114-WinForms/Program.cs
+++ P_Secu-114-WinForms/P_Secu-114-WinForms/Program.cs
@@ -12,15 +12,6 @@ namespace P_Secu_114_WinForms
         [STAThread]
         static void Main()
         {
-            MasterPassword.Key = "etml";
-            PasswordManager.PasswordList.Add(new Entry("1", "1", "1", "1"));
-            PasswordManager.PasswordList.Add(new Entry("2", "2", "1", "1"));
-            PasswordManager.PasswordList.Add(new Entry("3", "3", "1", "1"));
-            PasswordManager.PasswordList.Add(new Entry("4", "4", "1", "1"));
-            PasswordManager.PasswordList.Add(new Entry("5", "5", "1", "1"));
-            PasswordManager.PasswordList.Add(new Entry("6", "6", "1", "1"));
-            PasswordManager.PasswordList.Add(new Entry("7", "7", "1", "1"));
-            SaveFile.SaveEntries();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainMenu());
